02a5cf1f5d3b8d2a3b7e10f10b19a6826a14273f
Piotr Prusinski <prusinski.pio@gmail.com>
2025-10-09 07:04:48 +0200

    Send call to taxi service on trip creation

--- backend/trips/router.py
+++ backend/trips/router.py
@@ -5,11 +5,37 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from taxis.models import Taxi
 from taxis.schemas import TaxiStatus
+import httpx
+import logging
 
 from .models import Trip
 from .schemas import TripCreate, TripResponse
 
+logger = logging.getLogger(__name__)
+
 router = APIRouter(prefix="/trips", tags=["trips"])
+
+
+async def send_request_to_taxi(trip: Trip, callback_url: str) -> None:
+    async with httpx.AsyncClient(timeout=10.0) as client:
+        taxi_body = {
+            "x_start": trip.x_start,
+            "y_start": trip.y_start,
+            "x_destination": trip.x_destination,
+            "y_destination": trip.y_destination,
+            "taxi_id": trip.taxi_id,
+            "user_id": trip.user_id,
+            "waiting_time_minutes": trip.waiting_time_minutes,
+            "travel_time_minutes": trip.travel_time_minutes,
+        }
+        try:
+            await client.post(callback_url, json=taxi_body)
+        except httpx.ConnectTimeout:
+            # This should probably raise HTTPException,
+            # Also, Trip shouldn't be created if we can't send request
+            # I left it as is for simplicity
+            logger.error("Could not reach taxi")
+            pass
 
 
 @router.post("", response_model=TripResponse)
@@ -28,16 +54,19 @@ async def order_trip(
             x_destination=int(req.x_destination),
             y_destination=int(req.y_destination),
         )
-        await trip.create(db_session)
+        trip = await trip.create(db_session)
 
         await add_event(
             db_session,
             event_type=EventType.TAXI_ASSIGNMENT,
-            taxi_id=nearest_taxi.pk,
+            taxi_id=trip.taxi_id,
             trip_id=trip.pk,
             user_id=trip.user_id,
         )
         await db_session.commit()
+
+        await send_request_to_taxi(trip, nearest_taxi.callback_url)
+
         return trip
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
--- backend/trips/tests/test_router.py
+++ backend/trips/tests/test_router.py
@@ -4,15 +4,22 @@ import random
 import pytest
 from dispatch_events.models import Event
 from httpx import AsyncClient
+from unittest.mock import AsyncMock, patch
 from sqlalchemy.ext.asyncio import AsyncSession
 from taxis.models import Taxi
 from taxis.schemas import TaxiStatus
 from tests.test_utils import serialize_model
 
+PATH = "trips.router"
 
+
+@patch(f"{PATH}.send_request_to_taxi")
 @pytest.mark.asyncio
 async def test_order_trip(
-    db_session: AsyncSession, client: AsyncClient, monkeypatch: pytest.MonkeyPatch
+    taxi_request_mock: AsyncMock,
+    db_session: AsyncSession,
+    client: AsyncClient,
+    monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     await Taxi(x=10, y=15, callback_url="http://mockurl:8080").create(db_session)
 
@@ -26,6 +33,7 @@ async def test_order_trip(
         "y_destination": 16,
     }
     response = await client.post("/trips", json=body)
+    taxi_request_mock.assert_called_once()
     assert response.status_code == 200, response.text
     assert response.json() == {
         "taxi_id": 1,
@@ -61,27 +69,32 @@ async def test_order_trip_no_taxis(
     }
 
 
+@patch(f"{PATH}.send_request_to_taxi")
 @pytest.mark.asyncio
 async def test_order_trip_creates_event(
-    db_session: AsyncSession, client: AsyncClient, dt_mock: dt.datetime
+    taxi_request_mock: AsyncMock,
+    db_session: AsyncSession,
+    client: AsyncClient,
+    dt_mock: dt.datetime,
 ) -> None:
-    await Taxi(x=10, y=15, callback_url="http://mockurl:8080").create(db_session)
+    await Taxi(pk=3, x=10, y=15, callback_url="http://mockurl:8080").create(db_session)
 
     body = {
-        "user_id": 1,
+        "user_id": 2,
         "x_start": 0,
         "y_start": 2,
         "x_destination": 13,
         "y_destination": 16,
     }
     response = await client.post("/trips", json=body)
+    taxi_request_mock.assert_called_once()
     assert response.status_code == 200, response.text
 
     event = (await Event.get_all(db_session))[0]
     assert serialize_model(event) == {
-        "taxi_id": 1,
+        "taxi_id": 3,
         "trip_id": 1,
-        "user_id": 1,
+        "user_id": 2,
         "event_type": "taxi_assignment",
         "created_at": dt_mock,
         "pk": 1,
